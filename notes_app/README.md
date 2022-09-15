56fbdf5790b3c1d9ab254094090240ab83590d70
otto@Ubuntu22_04 <cia1099@gapp.nthu.edu.tw>
2022-09-15 09:03:30 +0800

    deployed note_app successfully

--- notes_app/README.md
+++ notes_app/README.md
@@ -1,16 +1,33 @@
 # notes_app
 
-A new Flutter project.
+A similar Google Keep application.
+Only completed frontend, this app do not use any backend.
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+- Visit this site: https://notes-app-0915.firebaseapp.com
 
-A few resources to get you started if this is your first Flutter project:
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+## How to change App Icon
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+- https://www.youtube.com/watch?v=drAeE4qKjDQ
+- [Icons8](https://icons8.com/)
+- [favicon.io](https://favicon.io/)
+
+先在Icons8下載想要的Icon，抓最大且是`.png`的圖檔，再用favicon得到一系列的Icon大小。
+
+針對web的圖標，在`/web`資料夾下有`index.html`和`manifest.json`要修改，`index.html`是修改網頁的圖標，`manifest.json`則是修改PWA用的圖標。
+
+1. 要修改網頁的Icon，先將從favicon下載的`favicon.ico`檔案放置在`/web`資料夾內，可以刪除`favicon.png`，然後修改`/web/index.html`:
+```html
+<!-- Favicon -->
+   <!-- 刪除 <link rel="shortcut icon" type="image/png" href="favicon.png"/> -->
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <link rel="icon" href="favicon.ico" type="image/x-icon">
+```
+
+2. 修改PWA的Icon，只要將從favicon下載的圖標在相同尺寸下，更改成和Flutter預設的檔名相同，然後再覆蓋到`/web/icons`即可。
+
+3. 注意在`MaterialApp`設定的Title不會和鏈接的標題一樣，還要在`index.html`和`manifest.json`改好對應的attributes。如果想有更完整的描述，可以再修改`description`attribute。
+
+<font color=ff00ff>如果`/web`改爛了，直接刪除再到終端輸入`flutter create .`重生一個預設的就好。</font>
--- notes_app/lib/main.dart
+++ notes_app/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_frame/flutter_web_frame.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:notes_app/app_colors.dart';
 import 'package:notes_app/views/landing_view.dart';
@@ -16,44 +18,50 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Provider(
-      create: ((context) => <Note>[]),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        themeMode: ThemeMode.dark,
-        darkTheme: ThemeData(
-            appBarTheme: const AppBarTheme(
-                actionsIconTheme:
-                    IconThemeData(color: AppColors.lightTextColor),
-                centerTitle: false,
-                backgroundColor: Colors.transparent,
-                shadowColor: Colors.transparent),
-            floatingActionButtonTheme: const FloatingActionButtonThemeData(
-                backgroundColor: AppColors.buttonColor),
-            textButtonTheme: TextButtonThemeData(
-                style: TextButton.styleFrom(
-                    foregroundColor: AppColors.lightTextColor)),
-            tabBarTheme: const TabBarTheme(
-                indicator: UnderlineTabIndicator(
-                    borderSide: BorderSide(width: 3, color: AppColors.orange)),
-                indicatorSize: TabBarIndicatorSize.label,
-                labelColor: AppColors.orange),
-            fontFamily: GoogleFonts.poppins().fontFamily,
-            backgroundColor: AppColors.containerBackground,
-            scaffoldBackgroundColor: AppColors.scaffoldBackground,
-            brightness: Brightness.dark,
-            elevatedButtonTheme: ElevatedButtonThemeData(
-                style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(100, 50),
-                    backgroundColor: AppColors.buttonColor,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30))))),
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+    return FlutterWebFrame(
+      builder: (context) => Provider(
+        create: ((context) => <Note>[]),
+        child: MaterialApp(
+          title: 'Notes App',
+          debugShowCheckedModeBanner: false,
+          themeMode: ThemeMode.dark,
+          darkTheme: ThemeData(
+              appBarTheme: const AppBarTheme(
+                  actionsIconTheme:
+                      IconThemeData(color: AppColors.lightTextColor),
+                  centerTitle: false,
+                  backgroundColor: Colors.transparent,
+                  shadowColor: Colors.transparent),
+              floatingActionButtonTheme: const FloatingActionButtonThemeData(
+                  backgroundColor: AppColors.buttonColor),
+              textButtonTheme: TextButtonThemeData(
+                  style: TextButton.styleFrom(
+                      foregroundColor: AppColors.lightTextColor)),
+              tabBarTheme: const TabBarTheme(
+                  indicator: UnderlineTabIndicator(
+                      borderSide:
+                          BorderSide(width: 3, color: AppColors.orange)),
+                  indicatorSize: TabBarIndicatorSize.label,
+                  labelColor: AppColors.orange),
+              fontFamily: GoogleFonts.poppins().fontFamily,
+              backgroundColor: AppColors.containerBackground,
+              scaffoldBackgroundColor: AppColors.scaffoldBackground,
+              brightness: Brightness.dark,
+              elevatedButtonTheme: ElevatedButtonThemeData(
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(100, 50),
+                      backgroundColor: AppColors.buttonColor,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30))))),
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const LandingView(),
         ),
-        home: const LandingView(),
       ),
+      maximumSize: const Size(475.0, 812.0),
+      enabled: kIsWeb,
+      backgroundColor: Colors.grey,
     );
   }
 }

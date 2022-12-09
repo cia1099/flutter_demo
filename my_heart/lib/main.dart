6fec5ea95245c115a953894150e34837e7317b83
ottolin <otto-tw@bobi.homes>
2022-12-09 18:30:23 +0800

    add fireworks to ready for happy new year celebration

--- my_heart/lib/main.dart
+++ my_heart/lib/main.dart
@@ -2,8 +2,10 @@ import 'dart:ui' as ui show Image;
 
 import 'package:flutter/material.dart';
 import 'package:my_heart/heart.dart';
+import 'package:my_heart/node/fireworks_node.dart';
 import 'package:my_heart/utilts.dart';
 import 'package:scratcher/scratcher.dart';
+import 'package:spritewidget/spritewidget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,6 +38,18 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    final scenery = Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.black,
+            Colors.blue[900]!,
+          ],
+        ),
+      ),
+    );
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -47,58 +61,57 @@ class _MyHomePageState extends State<MyHomePage> {
         //     borderColor: Color.fromARGB(255, 41, 17, 9),
         //     borderWith: 12,
         //     size: Size(880, 800)),
-        child: CustomPaint(
-          foregroundPainter: HeartPainter(
-            bodyColor: Theme.of(context).scaffoldBackgroundColor,
-            isShallow: true,
-          ),
-          child: Scratcher(
-            brushSize: 30,
-            threshold: 50,
-            color: Colors.grey,
-            rebuildOnResize: false,
-            onChange: (value) => print("Scratch progress: $value%"),
-            onThreshold: () => print("Threshold reached, you won!"),
-            child: Container(
-              height: 800,
-              width: 880,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: NetworkImage("https://picsum.photos/300/300"),
-                      fit: BoxFit.cover)),
-            ),
+        //===== Heart Text
+        // child: CustomPaint(
+        //   foregroundPainter: HeartPainter(
+        //     bodyColor: Theme.of(context).scaffoldBackgroundColor,
+        //     isShallow: true,
+        //   ),
+        //   child: Scratcher(
+        //     brushSize: 30,
+        //     threshold: 50,
+        //     color: Colors.grey,
+        //     rebuildOnResize: false,
+        //     onChange: (value) => print("Scratch progress: $value%"),
+        //     onThreshold: () => print("Threshold reached, you won!"),
+        //     child: Container(
+        //       height: 800,
+        //       width: 880,
+        //       decoration: BoxDecoration(
+        //           image: DecorationImage(
+        //               image: NetworkImage("https://picsum.photos/300/300"),
+        //               fit: BoxFit.cover)),
+        //     ),
+        //   ),
+        // ),
+        // === Happy New Year
+        child: Container(
+          width: 400,
+          height: 480,
+          child: FutureBuilder<ui.Image>(
+            future: dartDecodeImage("assets/firework-particle.png", context),
+            builder: (context, snapshot) {
+              return Stack(
+                children: [
+                  scenery,
+                  snapshot.connectionState == ConnectionState.waiting
+                      ? const CircularProgressIndicator.adaptive()
+                      : LayoutBuilder(
+                          builder: (context, constraints) {
+                            final parentSize = Size(
+                                constraints.maxWidth, constraints.maxHeight);
+                            return SpriteWidget(
+                              FireworksNode(
+                                  image: snapshot.data!,
+                                  resolution: parentSize),
+                            );
+                          },
+                        )
+                ],
+              );
+            },
           ),
         ),
-        // child: FutureBuilder<ui.Image>(
-        //   future: dartDecodeImage("https://picsum.photos/300/300", context),
-        //   builder: (context, snapshot) {
-        //     if (snapshot.connectionState == ConnectionState.waiting) {
-        //       return const CircularProgressIndicator.adaptive();
-        //     }
-        //     final img = snapshot.data!;
-        //     return CustomPaint(
-        //       foregroundPainter: ImagePaiter(
-        //           image: img,
-        //           wallColor: Theme.of(context).scaffoldBackgroundColor),
-        //       child: Scratcher(
-        //         brushSize: 30,
-        //         threshold: 50,
-        //         color: Colors.grey,
-        //         rebuildOnResize: false,
-        //         onChange: (value) => print("Scratch progress: $value%"),
-        //         onThreshold: () => print("Threshold reached, you won!"),
-        //         child: Container(
-        //           width: img.width.toDouble(),
-        //           height: img.height.toDouble(),
-        //           decoration: BoxDecoration(
-        //               image: DecorationImage(
-        //                   image: NetworkImage("https://picsum.photos/300/300"),
-        //                   fit: BoxFit.cover)),
-        //         ),
-        //       ),
-        //     );
-        // },
-        // ),
       ),
     );
   }
